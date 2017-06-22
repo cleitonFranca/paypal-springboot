b563f7a5f904de53de036edff53c7e6f9b8edb7c
Cleiton França <cleiton2281@gmail.com>
2017-06-21 23:23:52 -0300

    Signed-off-by: Cleiton França <cleiton2281@gmail.com>

--- src/test/java/TestCredicard.java
+++ src/test/java/TestCredicard.java
@@ -10,8 +10,10 @@ import com.paypal.api.payments.Amount;
 import com.paypal.api.payments.CreditCard;
 import com.paypal.api.payments.Details;
 import com.paypal.api.payments.FundingInstrument;
+import com.paypal.api.payments.Payee;
 import com.paypal.api.payments.Payer;
 import com.paypal.api.payments.Payment;
+import com.paypal.api.payments.Phone;
 import com.paypal.api.payments.Transaction;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.PayPalRESTException;
@@ -34,41 +36,55 @@ public class TestCredicard {
 		
 		// Set address info
 		Address billingAddress = new Address();
-		billingAddress.setCity("Johnstown");
-		billingAddress.setCountryCode("US");
-		billingAddress.setLine1("52 N Main ST");
-		billingAddress.setPostalCode("43210");
-		billingAddress.setState("OH");
+		billingAddress.setCity("Natal");
+		billingAddress.setCountryCode("BR");
+		billingAddress.setLine1("Rua Vereador Sergio Dieb, 234");
+		billingAddress.setPostalCode("59150000");
+		billingAddress.setState("RN");
 
 		// Credit card info
 		CreditCard creditCard = new CreditCard();
 		creditCard.setBillingAddress(billingAddress);
 		creditCard.setExpireMonth(11);
 		creditCard.setExpireYear(2018);
-		creditCard.setFirstName("Joe");
-		creditCard.setLastName("Shopper");
+		creditCard.setFirstName("Wander");
+		creditCard.setLastName("Cleiton");
 		creditCard.setNumber("4669424246660779");
+		creditCard.setCvv2(123);
 		creditCard.setType("visa");
 
 		// Payment details
 		Details details = new Details();
-		details.setShipping("1");
-		details.setSubtotal("5");
-		details.setTax("1");
+		//details.setShipping("1");
+		details.setSubtotal("6");
+		
+		//details.setTax("1");
 		details.setFee("free");
 	
 
 		// Total amount
 		Amount amount = new Amount();
 		amount.setCurrency("USD");
-		amount.setTotal("7");
+		amount.setTotal("6");
 		amount.setDetails(details);
 
+		//
+//		Phone phone = new Phone();
+//		phone.setCountryCode("BR");
+//		phone.setNationalNumber("84 36145962");
+//		
+		//
+		//Payee p = new Payee();
+		//p.setEmail("teste@teste.com");
+		//p.setFirstName("Wander");
+//		p.setPhone(phone);
+		
 		// Transaction details
 		Transaction transaction = new Transaction();
 		transaction.setAmount(amount);
-		transaction.setDescription("This is the payment transaction description.");
 		
+		transaction.setDescription("This is the payment transaction description.");
+		//transaction.setPayee(p);
 
 		List<Transaction> transactions = new ArrayList<Transaction>();
 		transactions.add(transaction);
@@ -84,6 +100,7 @@ public class TestCredicard {
 		Payer payer = new Payer();
 		payer.setFundingInstruments(fundingInstrumentList);
 		payer.setPaymentMethod("credit_card");
+		
 		
 		
 		
